2419eb375d3063d4fc4ed256503143fe98e9d1d6
Aron Mandrella <aron.mandrella@gmail.com>
2023-11-14 10:33:29 +0100

    run test build

---
+++ docs/_next/static/cBH2Gk2jrG_LmquYehOHH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-5d4aaa4065bf6a99.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
